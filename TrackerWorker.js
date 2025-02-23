7f6ba0a8bce21afda20acb6a8110c7a7edc83abc
Raju Basak <63355575+raju-gitlab@users.noreply.github.com>
2025-02-23 12:07:30 -0800

    Update TrackerWorker.js

--- TrackerWorker.js
+++ TrackerWorker.js
@@ -3,13 +3,24 @@
         window.TrackerWorkerService = {
             APIKEY : '',
             AppId : '',
-            URL : 'http://localhost:5278/',
-            Send : function(data) {
+            URL : 'https://0j56q6pg-5278.inc1.devtunnels.ms/',
+            TSend : function(data) {
                 fetch ('', {
-
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'content-type': 'application/json',
+                        'aId' : localStorage.getItem('__aId'),
+                        'uId' : localStorage.getItem('__uId'),
+                        'AppName' : 'Test',
+                        'Signature' : 'test',
+                    }
                 })
                 .then((res) => {
-
+                    console.log(res);
+                })
+                .catch((err) => {
+                    console.error(err);
                 })
             },
             Init : function () {
